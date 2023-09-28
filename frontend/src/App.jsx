b1b3580c6462ad79a17b1eaff1a8208ba990bf76
Odafe Alaiya <alaiyaodafe@gmail.com>
2023-09-28 13:13:16 +0100

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import HomePage from './pages/main_page/main'
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/main_page/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from "./main.scss"
+
+const HomePage = () => {
+  return (
+    <div className={styles.home_container}>
+      <div>
+        <h1>
+          Welcome to FlowBox!
+        </h1>
+        <p>
+          Create and manage a DAO easily with AI...
+        </p>
+      </div>
+      <div>
+        <div>
+          <button>
+            
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
